7287339d90bcad450ebe9022a7b04f0883c0d925
Felix Arnold <moin@felix-arnold.dev>
2020-09-30 15:18:22 +0200

    some tuning

--- siro/test.py
+++ siro/test.py
@@ -1,14 +1,13 @@
 import socket
+import const
 
-multicast_group = '238.0.0.18'
-server_address = ('', 32101)
-
+multicast_group = const.MULTICAST_GRP
+server_address = ('', const.CALLBACK_PORT)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 sock.bind(server_address)
 
-mreq = socket.inet_aton(multicast_group) + socket.inet_aton("10.0.0.187")
+mreq = socket.inet_aton(const.MULTICAST_GRP) + socket.inet_aton("10.0.0.187")
 
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
