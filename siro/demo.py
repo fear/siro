08018b7ea3eb5bf2d2a6e6483417421167e22f7b
Felix <moin@felix-arnold.dev>
2022-01-13 00:56:11 +0000

    clean demo script

--- siro/demo.py
+++ siro/demo.py
@@ -1,10 +1,6 @@
 #!python3
 
-import json
-
 from siro import (
-    RadioMotor,
-    Bridge,
     Connector
 )
 
@@ -14,25 +10,5 @@ def cli_demo(key_, addr_="") -> None:
     Connector().start_cli(key_, addr_)
 
 
-# noinspection PyShadowingNames
-def class_usage_demo(key_) -> None:
-    bridge: Bridge = Connector.bridge_factory(key_)
-
-    bridge.print_device_info()
-    devices = bridge.get_devices()
-    device: RadioMotor = devices[0]
-
-    device.move_up()
-    device.move_down()
-    device.move_to_position(40)
-    device.move_to_position(35)
-    device.move_stop()
-    device.get_status()
-
-
 if __name__ == '__main__':
-    config_ = json.load(open('config.json'))
-    key_ = config_['key']
-
-    cli_demo(key_)
-    # class_usage_demo(key)
+    cli_demo('30b9217c-6d18-4d')
