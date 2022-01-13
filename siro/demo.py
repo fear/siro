07a41cbc55b50cde685dfebc58d08a0d75b2fb29
Felix <moin@felix-arnold.dev>
2022-01-13 01:03:45 +0000

    clean demo script

--- siro/demo.py
+++ siro/demo.py
@@ -1,6 +1,5 @@
 #!python3
 
-import json
 import asyncio
 
 from siro import (
@@ -13,11 +12,11 @@ from siro import (
 # noinspection PyShadowingNames
 async def class_usage_demo(key_, loop) -> None:
     bridge: Bridge = await Helper.bridge_factory(key_, loop=loop)
-    listen = asyncio.create_task(bridge.listen(loop))
 
-    await listen
+    await asyncio.create_task(bridge.listen(loop))
 
     device = bridge.get_device_by_mac('98f4ab8932a40008')
+
     print('Warte 2 Sekunden')
     await asyncio.sleep(2)
 
@@ -42,16 +41,15 @@ async def class_usage_demo(key_, loop) -> None:
     print('Warte 1 Sekunden')
     await asyncio.sleep(1)
 
-    print('Fahre auf position 10% geschlossen')
-    device.move_to_position(10)
+    print('Fahre auf position 50%')
+    device.move_to_position(50)
+
 
 if __name__ == '__main__':
-    config_ = json.load(open('config.json'))
-    key_ = config_['key']
-
     loop = asyncio.get_event_loop()
-    future = loop.create_task(
-        class_usage_demo(key_, loop)
-    )
-    loop.run_until_complete(future)
 
+    future = loop.create_task(
+        class_usage_demo('30b9217c-6d18-4d', loop)
+    )
+
+    loop.run_until_complete(future)
